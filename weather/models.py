de88f8ed1aa7e987415532380fadcaa98ced70e5
Roman Pikulenko <roman.pikulenko@gmail.com>
2023-06-07 23:34:40 +0300

    Added api call to openweather Added displaying cities' weather

--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+class City(models.Model):
+    """Model definition for City."""
+
+    name = models.CharField(max_length=25)
+
+    class Meta:
+        """Meta definition for City."""
+
+        verbose_name = "City"
+        verbose_name_plural = "Cities"
+
+    def __str__(self):
+        return self.name
--- weather/views.py
+++ weather/views.py
@@ -1,8 +1,41 @@
+import requests
+from django.conf import settings
 from django.shortcuts import render
+
+from weather.forms import CityForm
+
+from .models import City
 
 
 # Create your views here.
 def index(request):
-    context = {}
+    openweather_url = r"https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=%s" % (
+        settings.OPENWEATHER_API_KEY,
+    )
+
+    if request.method == "POST":
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm()
+
+    cities = City.objects.all().order_by("name")
+    weather_data = []
+
+    for city in cities:
+        r = requests.get(openweather_url.format(city)).json()
+
+        city_weather = {
+            "city": city.name,
+            "temperature": r["main"]["temp"],
+            "description": r["weather"][0]["description"],
+            "icon": r["weather"][0]["icon"],
+        }
+        weather_data.append(city_weather)
+
+    context = {
+        "weather_data": weather_data,
+        "form": form,
+    }
 
     return render(request, "weather/weather.html", context)
