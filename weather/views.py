eb62bd2b1b10776fb243b879c150a4ba7e9851b4
Roman Pikulenko <roman.pikulenko@gmail.com>
2023-06-08 11:30:03 +0300

    Added handling of duplicates Sort order is case insansitive

--- weather/views.py
+++ weather/views.py
@@ -1,5 +1,6 @@
 import requests
 from django.conf import settings
+from django.db.models.functions import Lower
 from django.shortcuts import render
 
 from weather.forms import CityForm
@@ -13,13 +14,23 @@ def index(request):
         settings.OPENWEATHER_API_KEY,
     )
 
+    err_msg = ""
+
     if request.method == "POST":
         form = CityForm(request.POST)
-        form.save()
+
+        if form.is_valid():
+            new_city = form.cleaned_data["name"]
+            existing_city_count = City.objects.filter(name__icontains=new_city).count()
+
+            if not existing_city_count:
+                form.save()
+            else:
+                err_msg = f"The city {new_city} alredy exists"
 
     form = CityForm()
 
-    cities = City.objects.all().order_by("name")
+    cities = City.objects.all().order_by(Lower("name"))
     weather_data = []
 
     for city in cities:
